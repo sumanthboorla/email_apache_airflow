d9fe3b7b3c04695b94c25c283c68f2dd03e12896
SUMANTH BOORLA <73389805+sumanthboorla@users.noreply.github.com>
2025-06-29 14:26:15 +0530

    Delete dags/smtptest.py

--- dags/smtptest.py
+++
@@ -1,6 +0,0 @@
-import smtplib
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login('sumanthb.freelance@gmail.com', 'yehlkabrdbolaryw')
-server.sendmail('sumanthb.freelance@gmail.com', 'sumanthb31@gmail.com', 'Subject: Test\n\nHello123')
-server.quit()
